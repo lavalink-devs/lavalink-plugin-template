47e0db16190dececb965b84ab602d06771a7072c
Michael Rittmeister <dr@schlau.bi>
2023-07-04 13:28:21 +0000

    Reformat code

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 group = "org.example"
 version = "0.1.0"
 
-lavalinkPlugin{
+lavalinkPlugin {
     name = "plugin-template"
     apiVersion = libs.versions.lavalink.api
     serverVersion = gitHash(libs.versions.lavalink.server)
